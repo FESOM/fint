71e7481577903723d307a8aee90e79f93dc79fcc
Nikolay Koldunov <koldunovn@gmail.com>
2022-05-02 20:36:39 +0200

    masking with file works

--- fint.py
+++ fint.py
@@ -1,7 +1,11 @@
 import xarray as xr
 import cartopy
 import numpy as np
-from scipy.interpolate import CloughTocher2DInterpolator, LinearNDInterpolator, NearestNDInterpolator
+from scipy.interpolate import (
+    CloughTocher2DInterpolator,
+    LinearNDInterpolator,
+    NearestNDInterpolator,
+)
 import matplotlib.tri as mtri
 import matplotlib.pylab as plt
 import pandas as pd
@@ -14,6 +18,7 @@ from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationB
 import gc
 import argparse
 from regions import define_region
+
 
 def lon_lat_to_cartesian(lon, lat, R=6371000):
     """
@@ -59,6 +64,7 @@ def create_indexes_and_distances(model_lon, model_lat, lons, lats, k=1, workers=
 
     return distances, inds
 
+
 def ind_for_depth(depth, depths_from_file):
     """
     Find the model depth index that is closest to the required depth
@@ -78,36 +84,66 @@ def ind_for_depth(depth, depths_from_file):
     dind = i
     return dind
 
+
 def load_mesh(mesh_path):
     nodes = pd.read_csv(
-            mesh_path+'/nod2d.out',
-            delim_whitespace=True,
-            skiprows=1,
-            names=["node_number", "x", "y", "flag"],
-        )
+        mesh_path + "/nod2d.out",
+        delim_whitespace=True,
+        skiprows=1,
+        names=["node_number", "x", "y", "flag"],
+    )
 
     x2 = nodes.x.values
     y2 = nodes.y.values
 
     file_content = pd.read_csv(
-            mesh_path+'/elem2d.out',
-            delim_whitespace=True,
-            skiprows=1,
-            names=["first_elem", "second_elem", "third_elem"],
-        )
+        mesh_path + "/elem2d.out",
+        delim_whitespace=True,
+        skiprows=1,
+        names=["first_elem", "second_elem", "third_elem"],
+    )
 
     elem = file_content.values - 1
 
     return x2, y2, elem
 
-def interpolate_kdtree2d(data_in, x2, y2, elem, lons, lats, distances, inds, radius_of_influence=100000):
-    
+
+def get_no_cyclic(x2, elem):
+    """Compute non cyclic elements of the mesh."""
+    d = x2[elem].max(axis=1) - x2[elem].min(axis=1)
+    no_cyclic_elem = np.argwhere(d < 100)
+    return no_cyclic_elem.ravel()
+
+
+def interpolate_kdtree2d(
+    data_in, x2, y2, elem, lons, lats, distances, inds, radius_of_influence=100000
+):
+
     interpolated = data_in[inds]
     interpolated[distances >= radius_of_influence] = np.nan
     interpolated[interpolated == 0] = np.nan
     interpolated.shape = lons.shape
-    
+
     return interpolated
+
+
+def mask_triangulation(data_in, triang2, elem, no_cyclic_elem):
+    data_in_on_elements = data_in[elem[no_cyclic_elem]].mean(axis=1)
+    data_in_on_elements[data_in_on_elements == 0] = -999
+    mmask = data_in_on_elements == -999
+    data_in[data_in == 0] = np.nan
+    triang2.set_mask(mmask)
+    return triang2
+
+
+def interpolate_triangulation(
+    data_in, triang2, trifinder, x2, y2, lon2, lat2, elem, no_cyclic_elem
+):
+    interpolated = mtri.LinearTriInterpolator(triang2, data_in, trifinder=trifinder)(
+        lon2, lat2
+    )
+    return interpolated
+
 
 def parse_depths(depths, depths_from_file):
     if len(depths.split(",")) > 1:
@@ -133,6 +169,7 @@ def parse_depths(depths, depths_from_file):
     # print(realdepths)
     return dinds, realdepths
 
+
 def parse_timesteps(timesteps, time_shape):
 
     if len(timesteps.split(":")) == 2:
@@ -156,6 +193,7 @@ def parse_timesteps(timesteps, time_shape):
     timesteps = y
     print("timesteps {}".format(timesteps))
     return timesteps
+
 
 def fint():
     parser = argparse.ArgumentParser(
@@ -223,7 +261,21 @@ def fint():
         help="Map boundaries in -180 180 -90 90 format that will be used for interpolation.",
         # metavar=("LONMIN", "LONMAX", "LATMIN", "LATMAX"),
     )
+    parser.add_argument(
+        "--interp",
+        choices=["nn", "mtri_linear"],  # "idist", "linear", "cubic"],
+        default="nn",
+        help="Interpolation method. Options are nn - nearest neighbor (KDTree implementation, fast), idist - inverse distance (KDTree implementation, decent speed), linear (scipy implementation, slow) and cubic (scipy implementation, slowest and give strange results on corarse meshes).",
+    )
 
+    parser.add_argument(
+        "--mask",
+        # nargs=4,
+        type=str,
+        # default="-180.0, 180.0, -80.0, 90.0",
+        help="Map boundaries in -180 180 -90 90 format that will be used for interpolation.",
+        # metavar=("LONMIN", "LONMAX", "LATMIN", "LATMAX"),
+    )
 
     args = parser.parse_args()
     data = xr.open_dataset(args.data)
@@ -232,7 +284,14 @@ def fint():
 
     variable_name = list(data.data_vars)[0]
     dim_names = list(data.coords)
-        
+    interpolation = args.interp
+    mask_file = args.mask
+
+    if mask_file is not None:
+        mask_data = xr.open_dataset(mask_file)
+        mask_variable_name = list(mask_data.data_vars)[0]
+        mask_data = mask_data[mask_variable_name]
+
     if ("nz1" in data.dims) or ("nz" in data.dims):
         depth_coord = dim_names[0]
         depths_from_file = data[depth_coord].values
@@ -257,25 +316,52 @@ def fint():
 
     x, y, lon, lat = define_region(args.box, args.res, projection)
 
-    depth_limit_up = 0
-    depth_limit_down = 2
-    time_start = 0
-    time_end = 5
-    distances, inds = create_indexes_and_distances(x2, y2, lon, lat, k=1, workers=4)
+    if interpolation == "mtri_linear":
+        no_cyclic_elem = get_no_cyclic(x2, elem)
+        triang2 = mtri.Triangulation(x2, y2, elem[no_cyclic_elem])
+        trifinder = triang2.get_trifinder()
+    elif interpolation == "nn":
+        distances, inds = create_indexes_and_distances(x2, y2, lon, lat, k=1, workers=4)
+
     interpolated3d = np.zeros((len(timesteps), len(realdepths), len(y), len(x)))
     for t_index, ttime in enumerate(timesteps):
         for d_index, (dind, realdepth) in enumerate(zip(dinds, realdepths)):
             print(ttime)
             data_in = data[variable_name][ttime, dind, :].values
-            interpolated = interpolate_kdtree2d(data_in, x2, y2, elem, lon, lat, distances, inds, radius_of_influence=radius_of_influence)
+            if interpolation == "mtri_linear":
+                if mask_file is None:
+                    triang2 = mask_triangulation(data_in, triang2, elem, no_cyclic_elem)
+                interpolated = interpolate_triangulation(
+                    data_in, triang2, trifinder, x2, y2, lon, lat, elem, no_cyclic_elem
+                )
+            elif interpolation == "nn":
+                interpolated = interpolate_kdtree2d(
+                    data_in,
+                    x2,
+                    y2,
+                    elem,
+                    lon,
+                    lat,
+                    distances,
+                    inds,
+                    radius_of_influence=radius_of_influence,
+                )
+            if mask_file is not None:
+                mask_level = mask_data[0, dind, :, :].values
+                mask = np.ma.masked_invalid(mask_level).mask
+                interpolated[mask] = np.nan
+
             interpolated3d[t_index, d_index, :, :] = interpolated
 
-    out1 = xr.Dataset({variable_name:(['time', 'depth', 'lat', 'lon'], interpolated3d)},
-                     coords={'time':np.atleast_1d(data.time.data[timesteps]),
-                    'depth':realdepths,
-                  'lon':(['lon'], x),
-                  'lat':(['lat'], y )
-                 })
+    out1 = xr.Dataset(
+        {variable_name: (["time", "depth", "lat", "lon"], interpolated3d)},
+        coords={
+            "time": np.atleast_1d(data.time.data[timesteps]),
+            "depth": realdepths,
+            "lon": (["lon"], x),
+            "lat": (["lat"], y),
+        },
+    )
 
     out1.to_netcdf(args.data.replace(".nc", "_interpolated.nc"))
 
@@ -286,5 +372,3 @@ if __name__ == "__main__":
     # args = parser.parse_args()
     # args.func(args)
     fint()
-
-
